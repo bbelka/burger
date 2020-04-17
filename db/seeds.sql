4d201d9318a5d4b03bf42e7807385f9f4675a79a
bbtwots <brettbelka@gmail.com>
2020-04-16 18:01:07 -0700

    mostly done needs some formatting

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Mushroom swiss", false),
+("Bacon cheeseburger", false),
+("The Impossible", true),
+("Black bean", false),
+("Beyond Burger", true),
+("Tavern burger", false);
